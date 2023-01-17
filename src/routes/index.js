6ed3246a2056448613aa0a647369fb765a50630b
Manas <shuklamanasofficial@gmail.com>
2023-01-17 18:29:14 +0530

    minor change

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,10 @@
+const { loggerUtil } = require("../utils/logger")
 const auth = require("./auth")
 
 const routes = (app) => {
     // Test Route for API
     app.get("/welcome", (req, res) => {
+        loggerUtil("Welcome API called.")
         res.send("Welcome to API for Wellness Spoiled")
     })
 
