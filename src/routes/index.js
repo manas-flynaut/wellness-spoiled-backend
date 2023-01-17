3194502604a3b9a2285aa4b411b34030cfd94e9f
Manas <shuklamanasofficial@gmail.com>
2023-01-18 03:10:58 +0530

    Testing Action

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,7 +5,7 @@ const routes = (app) => {
     // Test Route for API
     app.get("/welcome", (req, res) => {
         loggerUtil("Welcome API called.")
-        res.send("Welcome to API for Wellness Spoiled")
+        res.send("Welcome to API for Wellness Spoiled.\n Servers are Up and Running")
     })
 
     app.use("/v1/api" , auth)
